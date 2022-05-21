571d6a56271f9ac395fb05e046f35c2e319724c2
Ricky Ananda Pardomuan Sitorus <75471225+rickyananada@users.noreply.github.com>
2022-05-21 23:31:24 +0700

    Final Update
    test

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Models\Product;
+use App\Models\Order;
+use App\Models\Critic;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -24,7 +28,19 @@ class DashboardController extends Controller
     }
     public function index()
     {
-        return view('pages.admin.dashboard.main');
+        $product = Product::count();
+        $user = User::where('role','=','user')->count();
+        $order = Order::count();
+        $critic = Critic::count();
+        $product_percent = Product::where(Product::raw("(date(created_at))"),'!=', date('Y-m-d'))->count();
+        $user_percent = User::where('role','=','user')->where(User::raw("(date(created_at))"),'!=', date('Y-m-d'))->count();
+        $order_percent = Order::where(Order::raw("(date(created_at))"),'!=', date('Y-m-d'))->count();
+        $critic_percent = Critic::where(Critic::raw("(date(created_at))"),'!=', date('Y-m-d'))->count();
+        $increase_product = ($product_percent / $product) * 100;
+        $increase_order = ($order_percent / $order) * 100;
+        $increase_critic = ($critic_percent / $critic) * 100;
+        $increase_user = ($user_percent / $user) * 100;
+        return view('pages.admin.dashboard.main',compact('product','order','user','critic','increase_product','increase_order','increase_critic','increase_user'));
     }
 
     public function create()
